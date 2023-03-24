71afdf6c053f6bc8236454aaccf03de6f4001c01
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-03-24 03:03:42 +0200

    trying filament

---
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Pages;
+
+use App\Filament\Resources\ProductResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProduct extends CreateRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    public function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['price'] = $data['price'] * 100;
+        return $data;
+    }
+}
