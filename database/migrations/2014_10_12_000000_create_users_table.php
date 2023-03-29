dc5d7a6507b0d59f3f92653c85e0afd8b824ac29
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-03-29 20:58:36 +0200

    Roles & Permissions & Shield-Plugin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->foreignId('role_id')->nullable()->default(1)->constrained();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
